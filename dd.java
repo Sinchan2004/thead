b90de7138e801524b15927148245de55fab62da1
Sinchan2004 <160880860+Sinchan2004@users.noreply.github.com>
2025-03-04 15:15:01 +0530

    Create dd.java

---
+++ dd.java
@@ -0,0 +1,12 @@
+public class dd {
+    public static void main(String[] args) {
+        // Declare and initialize two numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
